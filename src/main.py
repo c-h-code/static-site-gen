81853a1857fab7ca61281e7d352d3043c124b999
c-h-code <connorhillis@protonmail.com>
2026-02-18 14:46:37 -0500

    added copy_static_and_push method to copy the static folder and move it to the public folder

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
+import shutil
 from textnode import TextNode, TextType
-
+import os
 
 def main() -> None:
+    shutil.rmtree("public/")
+    os.mkdir("public/")
+    copy_static_and_push("static/" , "public/")
+    
+def copy_static_and_push(copy_dir, dest_dir):
+    files = os.listdir(copy_dir)
+    for file in files:
+        if os.path.isfile(os.path.join(copy_dir, file)):
+            shutil.copy(os.path.join(copy_dir, file), dest_dir)
+        else:
+            os.mkdir(os.path.join(dest_dir,file))
+            copy_static_and_push(os.path.join(copy_dir, file), os.path.join(dest_dir,file))
 
-    text_node = TextNode("testing text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
 
 
+    
+    
 if __name__ == "__main__":
     main()
